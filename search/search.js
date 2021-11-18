a5971dbebbfb2276370a7f69d048d4d8114b56c8
SaravananDhandapaniA <91460255+SaravananDhandapaniA@users.noreply.github.com>
2021-11-18 12:21:26 +0530

    Update search.js

--- search/search.js
+++ search/search.js
@@ -24,72 +24,85 @@ function search(event) {
 
 const listOfVegetables = [
   {
+    id:1,
     name: "Drumstick",
     price: "25",
     url: "image/drumstick.jpg"
   },
   {
+    id:2,
     name: "Carrot",
     price: "15",
     url: "image/carrot.jpg"
   },
   {
+    id:3,
     name: "Beans",
     price: "20",
     url: "image/beans.jpg"
   },
   {
+    id:4,
     name: "Beetroot",
     price: "12",
     url: "image/beetroot.jpg"
 
   },
   {
+    id:5,
     name: "Cabbage",
     price: "30",
     url: "image/cabbage.jpg"
 
   },
   {
+    id:6,
     name: "Onion",
     price: "28",
     url: "image/onion.jpg"
   },
   {
+    id:7,
     name: "Tomato",
     price: "7",
     url: "image/tomato.jpg"
   },
   {
+    id:8,
     name: "Brinjal",
     price: "16",
     url: "image/brinjal.jpg"
   },
   {
+    id:9,
     name: "Capcicum",
     price: "40",
     url: "image/capcicum.jpg"
   },
   {
+    id:10,
     name: "Potato",
     price: "18",
     url: "image/potato.jpg"
   },
   {
+    id:11,
     name: "Pumpkin",
     price: "45",
     url: "image/pumpkin.jpg"
   },
   {
+    id:12,
     name: "BitterGuard",
     price: "14",
     url: "image/bitterguard.jpg"
   },
   {
+    id:13,
     name: "Califlower",
     price: "15",
     url: "image/califlower.jpg"
-  },
+  }
 ];
 
 
